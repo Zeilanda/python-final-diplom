ebed2970f943d4831301eb9b7e23724b504bdf4e
Zeilanda <83118578+Zeilanda@users.noreply.github.com>
2022-12-22 11:51:32 +0300

    adding models and yaml docker-compose

---
+++ orders/backend/models.py
@@ -0,0 +1,149 @@
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.validators import UnicodeUsernameValidator
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from backend.managers import CustomUserManager
+
+STATE_CHOICES = (
+    ('basket', 'Статус корзины'),
+    ('new', 'Новый'),
+    ('confirmed', 'Подтвержден'),
+    ('assembled', 'Собран'),
+    ('sent', 'Отправлен'),
+    ('delivered', 'Доставлен'),
+    ('canceled', 'Отменен'),
+)
+
+
+class User(AbstractUser):
+    # username_validator = UnicodeUsernameValidator()
+    email = models.EmailField(_('email address'), unique=True)
+
+    # username = models.CharField(
+    #     _('username'),
+    #     max_length=150,
+    #     help_text=_('Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.'),
+    #     validators=[username_validator],
+    #     error_messages={
+    #         'unique': _("A user with that username already exists."),
+    #     },
+    # )
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+# class Customer(models.Model):
+#
+#     city = models.CharField(max_length=30, verbose_name='Город', blank=True)
+#     street = models.CharField(max_length=30, verbose_name='Улица', blank=True)
+#     house = models.CharField(max_length=10, verbose_name='Дом', blank=True)
+#     phone = models.CharField(max_length=12, verbose_name='Телефон', blank=True)
+
+
+#
+#     class Meta:
+#         verbose_name = 'Покупатель'
+#         verbose_name_plural = "Список покупателей"
+#         ordering = ('email',)
+#
+#
+# class Shop(models.Model):
+#     name = models.CharField(max_length=50, verbose_name='Название')
+#     url = models.URLField(verbose_name='Ссылка', null=True, blank=True)
+#
+#
+# class Provider(AbstractUser):
+#     email = models.EmailField(_('email address'), unique=True)
+#     company = models.CharField(max_length=30, verbose_name='Компания', blank=True)
+#     shop = models.ForeignKey(Shop, verbose_name='Магазин', related_name='providers', blank=True,
+#                              on_delete=models.CASCADE)
+#     position = models.CharField(max_length=25, verbose_name='Должность', blank=True)
+#     username_validator = UnicodeUsernameValidator()
+#     username = models.CharField(
+#         _('username'),
+#         max_length=150,
+#         help_text=_('Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.'),
+#         validators=[username_validator],
+#         error_messages={
+#             'unique': _("A user with that username already exists."),
+#         },
+#     )
+#     USERNAME_FIELD = 'email'
+#     REQUIRED_FIELDS = []
+#     objects = CustomUserManager()
+#
+#     def __str__(self):
+#         return f'{self.first_name} {self.last_name}'
+#
+#     class Meta:
+#         verbose_name = 'Поставщик'
+#         verbose_name_plural = "Список поставщиков"
+#         ordering = ('email',)
+#
+#
+# class Category(models.Model):
+#     name = models.CharField(max_length=50, verbose_name='Название')
+#
+#     class Meta:
+#         verbose_name = 'Категория'
+#         verbose_name_plural = 'Список категорий'
+#         ordering = ('-name',)
+#
+#     def __str__(self):
+#         return self.name
+#
+#
+# class Product(models.Model):
+#     name = models.CharField(max_length=80, verbose_name='Название')
+#     model = models.CharField(max_length=50, verbose_name='Модель')
+#     external_id = models.PositiveIntegerField(verbose_name='Внешний ИД')
+#     price = models.PositiveIntegerField()
+#     price_rrc = models.PositiveIntegerField()
+#     quantity = models.PositiveIntegerField()
+#     category = models.ForeignKey(Category, verbose_name='Категория', related_name='products', blank=True,
+#                                  on_delete=models.CASCADE)
+#     shop = models.ForeignKey(Shop, verbose_name='Магазин', related_name='shop_product', blank=True,
+#                              on_delete=models.CASCADE)
+#
+#
+# class Parameter(models.Model):
+#     name = models.CharField(max_length=200)
+#     parameter = models.ManyToManyField(Product, through='ProductParameter')
+#
+#     def __str__(self):
+#         return self.name
+#
+#
+# class ProductParameter(models.Model):
+#     product = models.ForeignKey(Product, verbose_name='Продукт', blank=True,
+#                                 on_delete=models.CASCADE)
+#     parameter = models.ForeignKey(Parameter, verbose_name='Параметр', blank=True, on_delete=models.CASCADE)
+#     value = models.CharField(max_length=100, verbose_name='Значение')
+#
+#
+# class Order(models.Model):
+#     customer = models.ForeignKey(Customer, verbose_name='Покупатель', related_name='orders', blank=True,
+#                                  on_delete=models.CASCADE)
+#     status = models.CharField(max_length=15, verbose_name='Статус', choices=STATE_CHOICES)
+#     order_datetime = models.DateTimeField(auto_now_add=True)
+#     order_position = models.ManyToManyField(Product, through='OrderPosition')
+#
+#     class Meta:
+#         verbose_name = 'Заказ'
+#         verbose_name_plural = "Список заказ"
+#         ordering = ('-dt',)
+#
+#     def __str__(self):
+#         return str(self.order_datetime)
+#
+#
+# class OrderPosition(models.Model):
+#     order = models.ForeignKey(Order, verbose_name='Заказ', related_name='order_positions', blank=True,
+#                               on_delete=models.CASCADE)
+#     product = models.ForeignKey(Product, verbose_name='Товар', related_name='position_orders', blank=True,
+#                                 on_delete=models.CASCADE)
+#     amount = models.PositiveIntegerField()
---
+++ orders/backend/views.py
@@ -0,0 +1,34 @@
+# import yaml
+# from yaml import load as load_yaml, Loader
+# from django.shortcuts import render
+
+# Create your views here.
+# from backend.models import Shop, Category, Product, Parameter, ProductParameter
+
+# with open('orders/shop1.yaml') as stream:
+#     data = load_yaml(stream, Loader=Loader)
+#     try:
+#         shop = Shop.objects.get_or_create(name=data['shop'])
+#         for category in data['categories']:
+#             category_object, _ = Category.objects.get_or_create(id=category['id'], name=category['name'])
+#             category_object.shops.add(shop.id)
+#             category_object.save()
+#         Product.objects.filter(shop_id=shop.id).delete()
+#         for item in data['goods']:
+#             product, _ = Product.objects.get_or_create(external_id=item['id'],
+#                                                        name=item['name'],
+#                                                        category_id=item['category'],
+#                                                        model=item['model'],
+#                                                        price=item['price'],
+#                                                        price_rrc=item['price_rrc'],
+#                                                        quantity=item['quantity'],
+#                                                        shop_id=shop.id
+#                                                        )
+#
+#             for name, value in item['parameters'].items():
+#                 parameter_object, _ = Parameter.objects.get_or_create(name=name)
+#                 ProductParameter.objects.create(product_id=product.id,
+#                                                 parameter_id=parameter_object.id,
+#                                                 value=value)
+#     except yaml.YAMLError as exc:
+#         print(exc)
