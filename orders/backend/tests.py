8e950aec0b13abf7c125b32474fd9183c44ce831
Zeilanda <dar.remez@gmail.com>
2023-02-12 13:03:42 +0300

    add provider registration test

--- orders/backend/tests.py
+++ orders/backend/tests.py
@@ -1,13 +1,15 @@
+from django.core import mail
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
-class CustomerTestCase(APITestCase):
+
+
+class CustomerRegisterTestCase(APITestCase):
 
     # register_url = '/api/registration/customer'
     register_url = reverse('backend:register-customer')
     verify_email_url = reverse('backend:user-register-confirm')
     login_url = reverse('backend:user_login')
-
 
     def test_register(self):
 
@@ -29,3 +31,38 @@ class CustomerTestCase(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(response.json()["detail"], "Verification e-mail sent.")
 
+        # try to login - should fail, because email is not verified
+        login_data = {
+            "email": data["email"],
+            "password": data["password1"],
+        }
+        response = self.client.post(self.login_url, login_data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertTrue(
+            "A user with this email and password was not found." in response.json()["non_field_errors"]
+        )
+
+class ProviderRegisterTestCase(APITestCase):
+
+    register_url = reverse('backend:register-provider')
+    verify_email_url = reverse('backend:user-register-confirm')
+    login_url = reverse('backend:user_login')
+
+    def test_register(self):
+
+        # register data
+        data = {
+            "email": "xy-manager@example-email.com",
+            "password1": "verysecret1234",
+            "password2": "verysecret1234",
+            "first_name": "first_name",
+            "last_name": "last_name",
+            "shop": "XY-store",
+            "position": "manager",
+
+        }
+
+        response = self.client.post(self.register_url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.json()["detail"], "Verification e-mail sent.")
